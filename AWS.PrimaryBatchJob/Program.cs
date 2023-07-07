19d60f45ec3b4364d9117fa41127a7693fe74de4
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2023-07-07 16:30:14 +0530

    Picked config values and added user secrets. Able to generate 100 jobs in pipeline

---
+++ AWS.PrimaryBatchJob/Program.cs
@@ -0,0 +1,149 @@
+﻿using Amazon.Batch;
+using Amazon.Batch.Model;
+using Amazon.CloudWatchLogs;
+using Amazon.Runtime;
+using Amazon.S3;
+using AWS.S3.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.AwsCloudWatch;
+using System;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AWS.PrimaryBatchJob
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Build the configuration
+            var configuration = BuildConfiguration();
+
+            // Configure Serilog
+            ConfigureLogger(configuration);
+
+            // Build the service provider
+            var serviceProvider = BuildServiceProvider(configuration);
+
+            string? parameterValue = Environment.GetEnvironmentVariable("SampleParameterName");
+
+            Log.Information($"Environment value received - {parameterValue}");
+
+            #region Upload S3 file
+
+            //Log.Information("Upload S3 file started...");
+            //// Upload S3 file
+            //var amazonS3Service = serviceProvider.GetService<IAmazonS3Service>();
+            //string jsonString = "{\"Name\":\"John Doe\",\"Bio\":\"Software developer\",\"JoinDate\":\"2023-05-31T20:29:33-04:00\",\"Author\":true}";
+            //object jsonContent = JObject.Parse(jsonString);
+            //string json = JsonConvert.SerializeObject(jsonContent);
+            //byte[] bytes = Encoding.UTF8.GetBytes(json);
+            //try
+            //{
+            //    using (MemoryStream stream = new MemoryStream(bytes))
+            //    {
+            //        var response = await amazonS3Service.UploadFileAsync("Sample", stream);
+            //        Log.Information("Uploaded S3 file successfully");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.Information($"S3 file upload unsuccessful - {ex.Message}");
+            //}
+
+            #endregion
+
+            var awsBatchSettings = serviceProvider.GetService<AWSBatchSettings>();
+            await SubmitAnotherJobToAWSBatchAsync(parameterValue, awsBatchSettings);
+            await Log.CloseAndFlushAsync();
+        }
+
+        private static async Task SubmitAnotherJobToAWSBatchAsync(string? parameterValue, AWSBatchSettings? awsBatchSettings)
+        {
+            var accessKey = awsBatchSettings?.AccessKey;
+            var secretKey = awsBatchSettings?.SecretKey;
+            var region = awsBatchSettings?.Region;
+            var jobQueueArn = awsBatchSettings?.JobQueueArn;
+            var jobDefinition = awsBatchSettings?.JobDefinitionArn;
+
+            var credentials = new BasicAWSCredentials(accessKey, secretKey);
+            var batchClient = new AmazonBatchClient(credentials, Amazon.RegionEndpoint.GetBySystemName(region));
+
+            // Define a list of jobs with their respective parameters
+            var job = new SubmitJobRequest
+            {
+                JobDefinition = jobDefinition,
+                JobName = $"secondary_batch_job_{parameterValue}",
+                JobQueue = jobQueueArn,
+                ContainerOverrides = new ContainerOverrides
+                {
+                    //Send json data through environment variables
+                    Environment = new List<Amazon.Batch.Model.KeyValuePair>()
+                        {
+                            new Amazon.Batch.Model.KeyValuePair(){Name="SampleParameterName",Value= parameterValue}
+                        }
+                }
+            };
+            try
+            {
+                var response = await batchClient.SubmitJobAsync(job);
+                Console.WriteLine(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+
+        static IServiceProvider BuildServiceProvider(IConfiguration configuration)
+        {
+            var services = new ServiceCollection();
+
+            // Add the logging services
+            services.AddLogging(builder =>
+            {
+                builder.AddSerilog();
+            });
+            services.AddSingleton(configuration.GetSection("AWSBatch").Get<AWSBatchSettings>()!);
+            services.AddSingleton<IAmazonS3ClientFactory, AmazonS3ClientFactory>();
+            services.AddS3Config(configuration);
+            services.AddSingleton<IAmazonS3ClientFactory, AmazonS3ClientFactory>();
+            services.AddScoped<IAmazonS3Service, AmazonS3Service>();
+            // Add any other services you may need
+
+            // Build the service provider
+            return services.BuildServiceProvider();
+        }
+
+        static void ConfigureLogger(IConfiguration configuration)
+        {
+            string? logGroupName = configuration["Serilog:WriteTo:1:Args:logGroupName"];
+            string? region = configuration["Serilog:WriteTo:1:Args:region"];
+            Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Information()
+          .WriteTo.Console()
+          .WriteTo.AmazonCloudWatch(new CloudWatchSinkOptions
+          {
+              LogGroupName = logGroupName,
+              LogStreamNameProvider = new DefaultLogStreamProvider(),
+              TextFormatter = new Serilog.Formatting.Json.JsonFormatter(),
+              MinimumLogEventLevel = LogEventLevel.Information
+          }, new AmazonCloudWatchLogsClient(new AmazonCloudWatchLogsConfig { RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region) }))
+          .CreateLogger();
+            Log.Information("Sample job started...");
+        }
+    }
+}
--- AWS.SecondaryBatchJob/Program.cs
+++ AWS.SecondaryBatchJob/Program.cs
@@ -1,15 +1,11 @@
 ﻿using Amazon.CloudWatchLogs;
-using AWS.S3.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.AwsCloudWatch;
-using System.Text;
 
-namespace AWS.SimpleLinuxJob
+namespace AWS.SecondaryBatchJob
 {
     internal class Program
     {
@@ -23,31 +19,10 @@ namespace AWS.SimpleLinuxJob
 
             // Build the service provider
             var serviceProvider = BuildServiceProvider(configuration);
-            
+
             string? parameterValue = Environment.GetEnvironmentVariable("SampleParameterName");
 
-            Log.Information($"Environemnt value received - {parameterValue}");
-            Log.Information("Upload S3 file started...");
-
-            // Upload S3 file
-            var amazonS3Service = serviceProvider.GetService<IAmazonS3Service>();
-            string jsonString = "{\"Name\":\"John Doe\",\"Bio\":\"Software developer\",\"JoinDate\":\"2023-05-31T20:29:33-04:00\",\"Author\":true}";
-            object jsonContent = JObject.Parse(jsonString);
-            string json = JsonConvert.SerializeObject(jsonContent);
-            byte[] bytes = Encoding.UTF8.GetBytes(json);
-            try
-            {
-                using (MemoryStream stream = new MemoryStream(bytes))
-                {
-                    var response = await amazonS3Service.UploadFileAsync("Sample", stream);
-                    Log.Information("Uploaded S3 file successfully");
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Information($"S3 file upload unsuccessful - {ex.Message}");
-            }
-
+            Log.Information($"Environment value received from primary job - {parameterValue}");
             await Log.CloseAndFlushAsync();
         }
 
@@ -67,9 +42,6 @@ namespace AWS.SimpleLinuxJob
             {
                 builder.AddSerilog();
             });
-            services.AddS3Config(configuration);
-            services.AddSingleton<IAmazonS3ClientFactory, AmazonS3ClientFactory>();
-            services.AddScoped<IAmazonS3Service, AmazonS3Service>();
             // Add any other services you may need
 
             // Build the service provider
@@ -91,7 +63,7 @@ namespace AWS.SimpleLinuxJob
               MinimumLogEventLevel = LogEventLevel.Information
           }, new AmazonCloudWatchLogsClient(new AmazonCloudWatchLogsConfig { RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region) }))
           .CreateLogger();
-            Log.Information("Sample job started...");
+            Log.Information("Secondary job started...");
         }
     }
 }
